d9e5ce172881e0feeceadb345bd4376a38e862dc
allan <allan.sysdata@gmail.com>
2017-05-11 10:32:54 -0300

    primeira versao funcional com imagens

--- AcceptionReader/src/pages/home/home.ts
+++ AcceptionReader/src/pages/home/home.ts
@@ -1,7 +1,8 @@
 import { Component } from '@angular/core';
-import { NavController } from 'ionic-angular';
+import { NavController, LoadingController } from 'ionic-angular';
 import { Http } from '@angular/http';
 import 'rxjs/add/operator/map';
+
 
 @Component({
   selector: 'page-home',
@@ -10,15 +11,26 @@ import 'rxjs/add/operator/map';
 export class HomePage {
 
   public feeds: Array<string>;
-  private url: string = "https://www.reddit.com/new.json";  
+  private url: string = "https://www.reddit.com/new.json"; 
 
-  constructor(public navCtrl: NavController, public http: Http) {
+  constructor(public navCtrl: NavController, public http: Http, public loadingCtrl: LoadingController) {
 
-	  this.http.get(this.url).map(res => res.json())
-	      .subscribe(data => {
-	        this.feeds = data.data.children;
-	      }); 
+      this.fetchContent();
 
   }
+
+    fetchContent ():void {
+    	let loading = this.loadingCtrl.create({
+      		content: 'Fetching content...'
+    	});
+
+    	loading.present();
+
+	    this.http.get(this.url).map(res => res.json())
+	      .subscribe(data => {
+	        this.feeds = data.data.children;
+	        loading.dismiss();
+	      });  
+  	}
 
 }
